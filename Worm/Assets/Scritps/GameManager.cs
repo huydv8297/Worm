86ca5bf182f7dbef3f0d2f161ce9a063b05b619a
Duong Van Huy <huydv@rikkeisoft.com>
2020-03-31 10:22:29 +0700

    first commit

---
+++ Worm/Assets/Scritps/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	[SerializeField] Transform itemContainer;
+
+	public Transform[,] items;
+	public Color[] colors;
+	public int itemCount;
+
+	// Use this for initialization
+	void Start () {
+		itemCount = itemContainer.childCount;
+
+		int containerSize = (int) Mathf.Sqrt(itemCount);
+
+		items = new Transform[containerSize, containerSize];
+		var j = 0;
+		for(int i = 0; i < itemCount; i++){
+ 
+			items[j, i - j * containerSize] = itemContainer.GetChild(i);
+			if( (i + 1) % containerSize == 0){
+				j++;
+			}
+		}
+
+		Init();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Init(){
+		Image image;
+		foreach(Transform item in items){
+			image = item.GetComponent<Image>();
+			image.color = colors[Random.Range(0, 3)];
+		}
+	}
+}
